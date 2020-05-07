2d0a6cb599ccdb1692f847608e4d0d6f1275bc3d
Dzung Nguyen <ntd63t@gmail.com>
2020-05-07 23:41:43 +0700

    init

---
+++ lib/grape_entity_include/version.rb
@@ -0,0 +1,3 @@
+module GrapeEntityInclude
+  VERSION = "0.1.0"
+end
---
+++ spec/grape_entity_include_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GrapeEntityInclude do
+  it "has a version number" do
+    expect(GrapeEntityInclude::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
